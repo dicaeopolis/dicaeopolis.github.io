8b28c9e759481589915cb2c94041d16362382ea1
Li Yan <2634735762@qq.com>
2025-09-01 22:05:05 +0800

    Common update for content triggered at 2025-09-01 22:05:04 by update.sh

--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,9 @@
-# 欢迎来到Dicaeopolis' Wiki.
+---
+comments: true
+short_title: 欢迎
+---
+
+# 欢迎来到Dicaeopolis' Wiki
 
 本Wiki旨在提供一个交流技术和知识的平台。
 
--- update.sh
+++ update.sh
@@ -1,17 +1,26 @@
 echo "[+]Starting update process..."
+echo "[+]Adding all changes..."
+git add .
+
+echo "[+]Committing changes..."
+echo "  [+]Common update for content by update.sh"
+git commit -m "Common update for content triggered at $(date +"%Y-%m-%d %H:%M:%S") by update.sh"
 
 echo "[+]Constructing navigation automatically..."
 python3 ./auto_nav.py
 
 echo "[+]Please update the last edition time in Windows."
-# python3 ./get_last_edition_time.py
+python3 ./get_last_edition_time.py
 
-echo "[+]Adding all changes..."
-git add .
+echo "[+]Adding generated navigation..."
+git add mkdocs.yml
 
 echo "[+]Committing changes..."
-echo "  [+]Common update triggered at $(date +"%Y-%m-%d %H:%M:%S") by update.sh"
-git commit -m "Common update triggered at $(date +"%Y-%m-%d %H:%M:%S") by update.sh"
+echo "  [+]Common update for mkdocs.yml by update.sh"
+git commit -m "Common update for mkdocs.yml triggered at $(date +"%Y-%m-%d %H:%M:%S") by update.sh"
+
+echo "[+]Constructing navigation automatically..."
+python3 ./auto_nav.py
 
 echo "[+]Pushing to remote repo..."
 git push origin main
